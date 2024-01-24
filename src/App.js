c2fa4de7c6ccb1732bad1161a36498c5cf60c428
ahmedmbarki22 <mbarkiahmed490@gmail.com>
2024-01-24 09:31:50 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UserList from "./UserList";
+import "./App.css"; // You can style your app in App.css
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React API Example</h1>
       </header>
+      <main>
+        <UserList />
+      </main>
     </div>
   );
 }
